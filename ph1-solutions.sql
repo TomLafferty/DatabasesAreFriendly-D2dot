2519f59d70c61f44db731a2580279a9eb28daf65
Thomas Lafferty <tsl5046@gmail.com>
2022-01-11 12:38:03 -0500

    early solutions for ph1

--- ph1-solutions.sql
+++ ph1-solutions.sql
@@ -4,3 +4,23 @@
 USE sakila;
 
 -- Your solutions...
+
+# Which actors have the first name ‘Scarlett’
+SELECT * FROM actor WHERE first_name = 'Scarlett';
+
+# Which actors have the last name ‘Johansson’
+SELECT * FROM actor_info WHERE last_name = 'Johansson';
+
+# How many distinct actors last names are there?
+SELECT Count(DISTINCT last_name) FROM actor;
+
+# Which last names are not repeated?
+
+# Which last names appear more than once?
+# Which actor has appeared in the most films?
+# Is ‘Academy Dinosaur’ available for rent from Store 1?
+# Insert a record to represent Mary Smith renting ‘Academy Dinosaur’ from Mike Hillyer at Store 1 today .
+# When is ‘Academy Dinosaur’ due?
+# What is that average running time of all the films in the sakila DB?
+# What is the average running time of films by category?
+# Why does this query return the empty set?
